1bccae90f57543ac8786e8216b99b11cb529999d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-27 13:48:14 +0100

    fix: set origin branch name when pushing (#12)

--- src/parts/PublishBranch/PublishBranch.js
+++ src/parts/PublishBranch/PublishBranch.js
@@ -1,7 +1,9 @@
 import * as Exec from '../Exec/Exec.js'
+import * as GetBranchName from '../GetBranchName/GetBranchName.js'
 
-export const publishBranch = async ({ repositoryPath }) => {
-  await Exec.exec('git', ['push'], {
+export const publishBranch = async ({ repositoryPath, version }) => {
+  const branchName = GetBranchName.getBranchName(version)
+  await Exec.exec('git', ['push', '--set-upstream', 'origin', branchName], {
     cwd: repositoryPath,
   })
 }
